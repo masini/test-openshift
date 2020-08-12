60b00d40a03f9e522b8152342a7d5be1d6d3cf48
quarkusio <no-reply@quarkus.io>
2020-08-12 09:58:39 +0000

    Initial commit

---
+++ src/test/java/net/lucamasini/openshift/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package net.lucamasini.openshift;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
